2047d71097d84980e15153744b32d362402b4621
Archana <archysimats@gmail.com>
2020-09-29 20:36:00 +0530

    Second Commit: coding almost done

--- index.server.js
+++ index.server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const env = require('dotenv');
+const db = require("./config/mongoose");
+const passport = require("passport");
+const passportJWTStrategy = require("./config/passport-jwt-strategy");
 
 const app = express();
 
 env.config();
 
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", require("./routes"));
 app.get("/", (req, res, next) => {
   res.status(200).json({
     message: "Hello from server",
